092da977da4a89775173d9de8e0d8c27e347b187
amit <amitj@centillionsoftech.com>
2024-02-09 19:06:43 +0530

    add cron job

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require('./src/config')
-
+const cronJob = require('./src/utils/cron/user-cron')
 const routes = require('./src/routes')
 const server = express();
 
@@ -10,3 +10,7 @@ server.use('/api/v1',routes);
 server.listen(5000,()=>{
     console.log("server is calling in 5000 port")
 })
+process.on('SIGINT', () => {
+  cronJob.stop();
+  process.exit();
+});
